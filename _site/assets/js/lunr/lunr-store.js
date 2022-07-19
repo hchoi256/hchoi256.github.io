7b7bd2ab3cc21dd1cda27b8d908cc17ff062f411
hchoi256 <eric970412@gmail.com>
2022-07-20 07:31:05 +0900

    commit checking

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,10 +4,4 @@ var store = [{
         "tags": [],
         "url": "/hello/",
         "teaser": "/assets/images/echoi.jpg"
-      },{
-        "title": "template",
-        "excerpt":"[Notice] Get your post template!    공지사항입니다.       공지사항 순서1     공지사항 순서2    Home   #               Header 1  Header 1-1  Header 1-2   Header 2  Header 2-1  Header 2-2  ","categories": ["star"],
-        "tags": ["template"],
-        "url": "/star/template/",
-        "teaser": "/assets/images/echoi.jpg"
       }]
