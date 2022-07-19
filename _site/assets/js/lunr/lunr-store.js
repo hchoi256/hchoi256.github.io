fc02def36f266b73988b5324e4e46f325460f1d8
hchoi256 <eric970412@gmail.com>
2022-07-20 07:14:45 +0900

    import minimal mistakes

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Hello World",
+        "excerpt":"Third Project  Let’s go  ","categories": [],
+        "tags": [],
+        "url": "/hello/",
+        "teaser": "/assets/images/echoi.jpg"
+      },{
+        "title": "template",
+        "excerpt":"[Notice] Get your post template!    공지사항입니다.       공지사항 순서1     공지사항 순서2    Home               왜 안되ㅈ  Header 1  Header 1-1  Header 1-2   Header 2  Header 2-1  Header 2-2  ","categories": ["star"],
+        "tags": ["template"],
+        "url": "/star/template/",
+        "teaser": "/assets/images/echoi.jpg"
+      }]
