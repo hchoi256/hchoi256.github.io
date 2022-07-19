2b5b761a94e9e470b678aceab1e05f732916c137
hchoi256 <eric970412@gmail.com>
2022-07-20 07:20:01 +0900

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": "/assets/images/echoi.jpg"
       },{
         "title": "template",
-        "excerpt":"[Notice] Get your post template!    공지사항입니다.       공지사항 순서1     공지사항 순서2   커밋 Home               Header 1  Header 1-1  Header 1-2   Header 2  Header 2-1  Header 2-2  ","categories": ["star"],
+        "excerpt":"[Notice] Get your post template!    공지사항입니다.       공지사항 순서1     공지사항 순서2    Home   #               Header 1  Header 1-1  Header 1-2   Header 2  Header 2-1  Header 2-2  ","categories": ["star"],
         "tags": ["template"],
         "url": "/star/template/",
         "teaser": "/assets/images/echoi.jpg"
