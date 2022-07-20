90a49846c0a8b76f613e26fe81e4191c8d1a90a3
hchoi256 <eric970412@gmail.com>
2022-07-20 10:42:50 +0900

    nlp-basic-p1

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "NLP - Part 1: Text Mining and Tokenization",
         "excerpt":"PART 1: Text Mining 1-1) 텍스트 마이닝(Text Mining)이란? 비정형(= 구조화 되지 않은) 텍스트 빅 데이터에 대하여 효과적인 탐색 및 분석을 통해 유용한 정보, 패턴 및 실행 가능한 통찰력을 도출하는 과정이다. 텍스트 마이닝의 필요성 일반적으로 텍스트 데이터는 구조화가 되어있지 않아서 비지도학습이 활용된다. 하지만, 실생활에는 엄청나게 많은 양의 텍스트 데이터가 순간마다 생성되고...","categories": ["NLP"],
         "tags": ["NLP","python"],
-        "url": "/nlp/nlp-basic.md/",
+        "url": "/nlp/nlp-basic-p1/",
         "teaser": "/assets/images/echoi.jpg"
       }]
